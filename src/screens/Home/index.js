1d04dfd6727f77e80a983ee5aa73c5a91585e754
anthony <aalbertini95@gmail.com>
2019-07-31 23:51:08 +0200

    get hotels and display them

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,20 +1,77 @@
 import React from 'react';
-import { SafeAreaView, Button, View, Text } from 'react-native';
+import { SafeAreaView, View, FlatList } from 'react-native';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
+import * as hotelsActions from '../../redux/actions/hotels';
+import * as cartActions from '../../redux/actions/cart';
 import screensStyles from '../styles/screens';
+import HotelCard from '../../components/HotelCard';
+import Loader from '../../components/Loader';
 
-export default class Home extends React.Component {
+export class Home extends React.PureComponent {
+  componentDidMount() {
+    const { getHotels } = this.props;
+
+    getHotels();
+  }
+
+  _keyHotelItemCardExtractor = (item) => `hotelCard_${item.id}`;
+
+  _renderHotelItemCard = ({ item }) => {
+      const { navigation, addToCart, removeToCart, cart } = this.props;
+      const { hotelIds } = cart;
+      const isAddedToCart = hotelIds.includes(item.id);
+
+      return (
+        <HotelCard
+          hotel={item}
+          navigation={navigation}
+          addToCart={addToCart}
+          removeToCart={removeToCart}
+          isAddedToCart={isAddedToCart}
+        />
+      )
+  }
+
   render() {
-    return (
+    const { hotels } = this.props;
+    const { loading, success, list } = hotels;
+
+    if (loading) {
+      return <Loader loading />
+    }
+
+    return success ? (
       <SafeAreaView style={screensStyles.safeAreaView}>
         <View style={screensStyles.viewContainer}>
-          <Text>Home Screen</Text>
-          <Button
-            title="Go to Details"
-            onPress={() => this.props.navigation.navigate('HotelDetails')}
+          <FlatList
+            style={screensStyles.flatListContainer}
+            data={list}
+            keyExtractor={this._keyHotelItemCardExtractor}
+            renderItem={this._renderHotelItemCard}
           />
         </View>
       </SafeAreaView>
-    );
+    ) : null;
   }
 }
+
+Home.propTypes = {
+  getHotels: PropTypes.func.isRequired,
+  addToCart: PropTypes.func.isRequired,
+  removeToCart: PropTypes.func.isRequired,
+  hotels: PropTypes.object,
+};
+
+const mapStateToProps = ({ hotels, cart }) => ({
+  hotels,
+  cart,
+});
+const mapDispatchToProps = dispatch => ({
+  getHotels: () => dispatch(hotelsActions.getHotels()),
+  addToCart: (hotelId) => dispatch(cartActions.addToCart(hotelId)),
+  removeToCart: (hotelId) => dispatch(cartActions.removeToCart(hotelId)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
