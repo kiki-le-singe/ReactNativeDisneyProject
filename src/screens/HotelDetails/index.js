98c8fdf16cdc096284121190227578214fb8ebf5
anthony <aalbertini95@gmail.com>
2019-07-31 23:50:18 +0200

    prepare HotelDetails screen

--- src/screens/HotelDetails/index.js
+++ src/screens/HotelDetails/index.js
@@ -3,12 +3,12 @@ import { SafeAreaView, View, Text } from 'react-native';
 
 import screensStyles from '../styles/screens';
 
-export default class HotelDetails extends React.Component {
+export default class HotelDetails extends React.PureComponent {
   render() {
     return (
       <SafeAreaView style={screensStyles.safeAreaView}>
         <View style={screensStyles.viewContainer}>
-          <Text>HotelDetails Screen</Text>
+          <Text>HotelDetails Screen - {this.props.navigation.getParam('hotelId')}</Text>
         </View>
       </SafeAreaView>
     );
