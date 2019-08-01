531a3c3f96a300cfaf245e9727559d040274d84f
anthony <aalbertini95@gmail.com>
2019-08-01 13:09:50 +0200

    add images

---
+++ src/utils/images.js
@@ -0,0 +1,8 @@
+const images = {
+  hotel1: require('Apps/src/assets/images/hotel1.png'),
+  hotel2: require('Apps/src/assets/images/hotel2.png'),
+  hotel3: require('Apps/src/assets/images/hotel3.png'),
+  hotel4: require('Apps/src/assets/images/hotel4.png'),
+};
+
+export default images;
