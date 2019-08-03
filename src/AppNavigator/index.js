e4801ab14144479a9942dd664a71b000133ee987
anthony <aalbertini95@gmail.com>
2019-08-03 12:54:23 +0200

    refactor AppNavigator

--- src/AppNavigator/index.js
+++ src/AppNavigator/index.js
@@ -1,13 +1,41 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation';
 
+import colors from 'Apps/src/utils/colors';
+import HeaderRight from 'Apps/src/components/HeaderRight';
+import HeaderLeft from 'Apps/src/components/HeaderLeft';
 import HomeScreen from '../screens/Home';
 import HotelDetailsScreen from '../screens/HotelDetails';
 import CartDetailsModalScreen from '../screens/CartDetails';
 
+const navigationOptions = {
+  headerStyle: {
+    backgroundColor: colors.grey,
+  },
+  headerTintColor: colors.white,
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+};
+
 const MainStack = createStackNavigator(
   {
-    Home: HomeScreen,
-    HotelDetails: HotelDetailsScreen,
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: ({ navigation }) => {
+       return {
+         ...navigationOptions,
+         headerLeft: (<HeaderLeft navigation={navigation} />),
+         headerRight: (<HeaderRight navigation={navigation} />),
+       };
+     },
+    },
+    HotelDetails: {
+      screen: HotelDetailsScreen,
+      navigationOptions: {
+       ...navigationOptions,
+     },
+    },
   },
   {
     initialRouteName: 'Home',
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -3,9 +3,6 @@ import { SafeAreaView, View, FlatList } from 'react-native';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import colors from 'Apps/src/utils/colors';
-import HeaderRight from 'Apps/src/components/HeaderRight';
-import HeaderLeft from 'Apps/src/components/HeaderLeft';
 import * as hotelsActions from '../../redux/actions/hotels';
 import * as cartActions from '../../redux/actions/cart';
 import screensStyles from '../styles/screens';
@@ -13,21 +10,6 @@ import HotelCard from '../../components/HotelCard';
 import Loader from '../../components/Loader';
 
 export class Home extends React.PureComponent {
-  static navigationOptions = ({ navigation }) => {
-    return {
-      title: 'LOGO',
-      headerStyle: {
-        backgroundColor: colors.grey,
-      },
-      headerTintColor: colors.white,
-      headerTitleStyle: {
-        fontWeight: 'bold',
-      },
-      headerLeft: (<HeaderLeft navigation={navigation} />),
-      headerRight: (<HeaderRight navigation={navigation} />),
-    };
-  };
-
   componentDidMount() {
     const { getHotels } = this.props;
 
